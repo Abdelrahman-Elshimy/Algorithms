46480c150f1d0f6a07de5127764a9f0740b69e13
Abdelrahman-Elshimy <abdoelsimy123@gmail.com>
2021-02-06 13:16:31 +0200

    Selection Sort

---
+++ selection sort/app.ts
@@ -0,0 +1,13 @@
+// Selection Sort
+
+// ! Slow
+
+let arr: number[] = [4, 3, 42, 54, 11, 76];
+var last = 0;
+for (let index = 0; index < arr.length; index++) {
+  let min = 0;
+  if (min > arr[index]) {
+    min = arr[index];
+  }
+  console.log(min);
+}
