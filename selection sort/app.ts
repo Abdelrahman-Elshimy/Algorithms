e9822626e25f9329c335c37f0e08980e13c3e82e
Abdelrahman-Elshimy <abdoelsimy123@gmail.com>
2021-02-06 13:25:12 +0200

    Selection Sort

--- selection sort/app.ts
+++ selection sort/app.ts
@@ -1,6 +1,17 @@
 // Selection Sort
 // ? O(n^2)
 // ! Slow
+/**  > Disadvantages
+ ** 1- Slow Because is loaping all array many times
+ ** 2- O(n^2)
+ ** 3- if already sorted it still sort
+ **/
+/**  ? Advantages
+ ** 1- if we need sort fixed top
+ ** 2- if we have small array
+ ** 3- very simple
+ ** 4- Memory
+ **/
 var arr = [4, 300, 42, 54, 11, 76];
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
